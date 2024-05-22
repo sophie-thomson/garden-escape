04fe2730652f8fad26e21345d7ac1c9a90f8c4ca
Sophie Thomson <sophielbenoit@gmail.com>
2024-05-22 14:41:24 +0000

    Fix: Add docstring and run through CI linter

--- story_separator.py
+++ story_separator.py
@@ -2,6 +2,10 @@ from colorama import Fore
 
 
 def story_separator():
+    """
+    Creates ASCII art to demarcate different sections of narrative and improve
+    spacing for better user experience.
+    """
     print(Fore.RED + "          (*)                 (*)            \
     (*)     (*)         (*)")
     print(Fore.GREEN + r"""           |                   |                  |
