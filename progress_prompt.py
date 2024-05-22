d45adba8682945e44df48d0f39e426eb799adcf1
Sophie Thomson <sophielbenoit@gmail.com>
2024-05-22 14:38:45 +0000

    Fix: Add docstring and run through CI Linter

--- progress_prompt.py
+++ progress_prompt.py
@@ -1,3 +1,10 @@
 from colorama import Fore
+
+
 def progress_prompt():
+    """
+    Repeatable function to add a prompt for the player to hit enter
+    to continue the story.
+    """
     input(Fore.YELLOW + " Press enter to continue... \n")
+    
