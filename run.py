15b7311e4de67032dc7a6d93cfa51e26ff8dc734
Sophie Thomson <sophielbenoit@gmail.com>
2024-05-22 14:34:47 +0000

    Redact and review all docstrings

--- run.py
+++ run.py
@@ -10,13 +10,20 @@ import title as title
 
 def get_player_name():
     """
-    Function to get a chosen name from the user and run length validation.
+    Gets a chosen name from the player and runs validation to check length.
+
     Uses a while loop to repeatedly request data, until it is valid.
     Will capitalise whatever name the user enters.
+
+    Args:
+        None
+
+    Returns:
+        str: The chosen name of the player.
     """
     while True:
         user_name = input(Fore.WHITE+" Enter your chosen player name: \n")
-        player = user_name.capitalize()
+        player = user_name.capitalize()  # capitalises name entered by player
         if validate_player_name(player):  # player is the data to check
             break  # if the name is valid, break command stops the while loop
     print(Fore.YELLOW + f"\n Good luck {player}!\n")
@@ -24,17 +31,26 @@ def get_player_name():
     return player
 
 
-def validate_player_name(name):
+def validate_player_name(player_name):
     """
-    Checks that the player enters at least one character as a name.
-    Also checks that the length of the name is no more than 16 characters so it
-    will fit on the congratulations window if they make it to the end.
+    Checks that the player enters at least one character and no more than 16
+    characters to fit on the congratulations window if they get to the end.
 
-    The name can be anything they want, so there is no validation to check if
-    they have entered any sybmols or numbers.
+    Name can include any characters, numbers and symbols, so there is no
+    validation to check if they have entered a str / int / float.
+
+    Raises a ValueError if the player enters a value that is not between 1-16
+    characters.
+
+    Args:
+        player_name (str): The chosen name entered by the player.
+
+    Returns:
+        boolean: True if validation runs with no error.
+        boolean: False if ValueError triggered.
     """
     try:
-        length = len(name)
+        length = len(player_name)
         if length < 1 or length > 16:  # checks length of name
             raise ValueError(Fore.RED +
                   f"Name must be 1-16 characters. You entered {length}")
@@ -48,21 +64,35 @@ def validate_player_name(name):
 
 def start_adventure(player_name):
     """
-    Displays the intro text for the beginning of the story and prompts player
-    to confirm they are ready to start the choose-your-own adventure.
+    Displays the intro text for the beginning of the story.
+    Uses an input prompt with player name in f-string to confirm they are
+    ready to start the choose-your-own adventure.
+
+    Args:
+        player_name (str): The chosen name entered by the player.
+
+    Returns:
+        None
     """
     print(Fore.CYAN + " You'll need your wits about you to make it out alive!")
     print(Fore.WHITE + st.INTRO_TEXT)
     # no input validation required. Only hitting enter will continue program
     input(Fore.YELLOW +
-        f" Are you ready {player_name}? Press enter to continue... \n")
+          f" Are you ready {player_name}? Press enter to continue... \n")
 
 
 def get_story_choice():
     """
-    Function to get story choice input from player.
+    Gets story choice input from the player.
+
     Uses a while loop to collect a valid entry of a or b from the player via
     the terminal. The loop will repeatedly request data, until it is valid.
+
+    Args:
+        None
+
+    Returns:
+        str: Choice of a or b entered by the player
     """
     while True:
         print(Fore.WHITE + Style.NORMAL + f" Choose wisely...\n")
@@ -75,9 +105,16 @@ def get_story_choice():
 
 def validate_story_choice(value):
     """
-    Validates if the player enters a story choice value that is either a or b.
+    Checks that the player enters a story choice value that is either a or b.
     Inside the try, raises a ValueError if the player enters a value that is
     not a or b.
+
+    Args:
+        value (str): The story_choice (a or b) entered by the player.
+
+    Returns:
+        boolean: True if validation runs with no error.
+        boolean: False if ValueError triggered.
     """
     try:
         if value not in ["a", "b"]:
@@ -93,10 +130,12 @@ def validate_story_choice(value):
 
 def display_spider_story():
     """
-    Displays narrative text from story_text file to set the scene for spider
-    section. Displays options for player to choose between to direct the
-    narrative and runs the display_spider_story_choices function for the player
-    to make their choice.
+    Extracts narrative text from story_text file to set the scene for the
+    spider section.
+
+    Displays two options (a or b) for player to choose between to direct the
+    narrative of the story and runs the display_spider_story_choices function
+    (for the player to select and see the outcome of their choice.)
     """
     ss.story_separator()  # runs story_separator to demarcate narrative blocks
     print(Fore.WHITE + st.SPIDER_TEXT)
@@ -110,32 +149,37 @@ def display_spider_story():
 def display_spider_story_choices():
     """
     Runs get_story_choice function for player to enter their chosen story
-    option and for the choice to be validated (validate_story_choice).
+    option and for their choice to be validated (validate_story_choice).
 
-    If valid, the choice is returned to this function for the program to
-    display the next block of story text depending on the chosen outcome along
-    with some ASCII art.
+    The validated story_choice is returned to this function and an if/else
+    statement displays the relevant block of narrative from the story_text
+    file depending on the player's choice of a or b.
     """
     player_choice = get_story_choice()
     if player_choice == "a":
         sp.spider_ascii()  # displays spider ASCII Art
         print(Fore.WHITE + st.SPIDER_OPTION_A_TEXT)
         pp.progress_prompt()  # adds prompt input to press enter to continue
-        display_nested_spider_choices()
+        display_nested_spider_choices()  # shows nested set of options
     else:
         sp.spider_ascii()  # displays spider ASCII Art
         print(Fore.WHITE + st.SPIDER_OPTION_B_TEXT)
-        pp.progress_prompt()
+        pp.progress_prompt()  # adds prompt input to press enter to continue
 
 
 def display_nested_spider_choices():
     """
-    Runs get_story_choice function for player to enter their chosen story
-    option and for the choice to be validated (validate_story_choice).
+    Extracts narrative text from story_text and displays a nested set of
+    options (a or b) for the player to choose between as a result of their
+    first spider story_choice.
 
-    If valid, the choice is returned to this function for the program to
-    display the next block of story text depending on the chosen outcome with
-    some ASCII art.
+    Runs get_story_choice function for player to enter their chosen story
+    option from the nested set of story options, and for their choice to
+    be validated (validate_story_choice).
+
+    The validated story_choice is returned to this function and an if/else
+    statement displays the relevant block of narrative from the story_text
+    file depending on the player's choice of a or b.
     """
     ss.story_separator()
     print(Fore.WHITE + st.NESTED_SPIDER_OPTIONS_TEXT)
@@ -151,31 +195,36 @@ def display_nested_spider_choices():
         ss.story_separator()
         print(Fore.WHITE + st.SPIDER_OPTION_B2_TEXT)
 
-    pp.progress_prompt()
+    pp.progress_prompt()  # adds prompt input to press enter to continue
 
 
 def display_centipede_story():
     """
-    Displays narrative text from story_text file to set the scene for centipede
-    section. Displays options for player to choose between to direct the
-    narrative and runs the display_centipede_story_choices function for the
-    player to make their choice.
+    Extracts narrative text from story_text file to set the scene for the
+    centipede section.
+
+    Runs the display_centipede_story_choices function for the player to
+    view the centipede story choice options.
     """
     ss.story_separator()
     print(Fore.WHITE + st.CENTIPEDE_STORY_TEXT)
-    pp.progress_prompt()
+    pp.progress_prompt()  # adds prompt input to press enter to continue
 
     display_centipede_story_choices()
 
 
 def display_centipede_story_choices():
     """
-    Runs get_story_choice function for player to enter their chosen story
-    option and for the choice to be validated (validate_story_choice).
+    Extracts narrative text from story_text and displays two centipede
+    options (a or b) for the player to choose between.
 
-    If valid, the choice is returned to this function for the program to
-    display the next block of story text depending on the chosen outcome with
-    some ASCII art.
+    Runs get_story_choice function for player to enter their chosen story
+    option, and for their choice to be validated (validate_story_choice).
+
+    The validated story_choice is returned to this function and an if/else
+    statement displays the relevant block of narrative from the story_text
+    file or runs the game_over() function depending on the player's choice
+    of a or b.
     """
     ss.story_separator()
     print(Fore.WHITE + st.CENTIPEDE_OPTIONS_TEXT)
@@ -184,39 +233,44 @@ def display_centipede_story_choices():
 
     player_choice = get_story_choice()
     import centipede  # displays centipede ASCII Art
-    if player_choice == "a":
+    if player_choice == "a":  # checks if player_choice is 'a'
         print(Fore.WHITE + st.CENTIPEDE_OPTION_A_TEXT)
-        pp.progress_prompt()
+        pp.progress_prompt()  # adds prompt input to press enter to continue
         ss.story_separator()
         print(Fore.WHITE + st.CENTIPEDE_GAME_OVER_TEXT)
         game_over()
     else:
         print(Fore.WHITE + st.CENTIPEDE_OPTION_B_TEXT)
-        pp.progress_prompt()
+        pp.progress_prompt()  # adds prompt input to press enter to continue
 
 
 def display_rake_story():
     """
-    Displays narrative text from story_text file to set the scene for rake
-    section. Displays options for player to choose between to direct the
-    narrative and runs the display_spider_story_choices function for the player
-    to make their choice.
+    Extracts narrative text from story_text file to set the scene for the
+    rake section.
+
+    Runs the display_rake_story_choices function for the player to view the
+    rake story choice options.
     """
     ss.story_separator()
     print(Fore.WHITE + st.RAKE_STORY_TEXT)
-    pp.progress_prompt()
+    pp.progress_prompt()  # adds prompt input to press enter to continue
 
     display_rake_story_choices()
 
 
 def display_rake_story_choices():
     """
-    Runs get_story_choice function for player to enter their chosen story
-    option and for the choice to be validated (validate_story_choice).
+    Extracts narrative text from story_text and displays two rake story
+    options (a or b) for the player to choose between.
 
-    If valid, the choice is returned to this function for the program to
-    display the next block of story text depending on the chosen outcome with
-    some ASCII art.
+    Runs get_story_choice function for player to enter their chosen story
+    option, and for their choice to be validated (validate_story_choice).
+
+    The validated story_choice is returned to this function and an if/else
+    statement displays the relevant block of narrative from the story_text
+    file or runs the game_over() function depending on the player's choice
+    of a or b.
     """
     ss.story_separator()
     print(Fore.WHITE + st.RAKE_OPTIONS_TEXT)
@@ -227,24 +281,29 @@ def display_rake_story_choices():
     import rake  # displays centipede ASCII Art
     if player_choice == "a":
         print(Fore.WHITE + st.RAKE_OPTION_A_TEXT)
-        pp.progress_prompt()
+        pp.progress_prompt()  # adds prompt input to press enter to continue
         ss.story_separator()
         print(Fore.WHITE + st.RAKE_GAME_OVER_TEXT)
         game_over()
     else:
         print(Fore.WHITE + st.RAKE_OPTION_B_TEXT)
-        pp.progress_prompt()
-        display_nested_rake_choices()
+        pp.progress_prompt()  # adds prompt input to press enter to continue
+        display_nested_rake_choices()  # shows second, nested set of options
 
 
 def display_nested_rake_choices():
     """
-    Runs get_story_choice function for player to enter their chosen story
-    option and for the choice to be validated (validate_story_choice).
+    Extracts narrative text from story_text and displays a nested set of
+    options (a or b) for the player to choose between as a result of their
+    first rake story_choice.
 
-    If valid, the choice is returned to this function for the program to
-    display the next block of story text depending on the chosen outcome with
-    some ASCII art.
+    Runs get_story_choice function for player to enter their chosen story
+    option from the nested set of story options, and for their choice to
+    be validated (validate_story_choice).
+
+    The validated story_choice is returned to this function and an if/else
+    statement displays the relevant block of narrative from the story_text
+    file depending on the player's choice of a or b.
     """
     print(Fore.WHITE + st.NESTED_RAKE_OPTIONS_TEXT)
     print(Fore.MAGENTA + Style.BRIGHT + st.RAKE_OPTION_A2)
@@ -253,7 +312,7 @@ def display_nested_rake_choices():
     player_choice = get_story_choice()
     if player_choice == "a":
         print(Fore.WHITE + st.RAKE_OPTION_A2_TEXT)
-        pp.progress_prompt()
+        pp.progress_prompt()  # adds prompt input to press enter to continue
         print(Fore.WHITE + st.END_TEXT)
     else:
         print(Fore.WHITE + st.RAKE_OPTION_B2_TEXT)
@@ -282,9 +341,16 @@ def game_over():
 
 def get_restart_choice():
     """
-    Function to get choice input from player if they want to restart or finish.
-    Run a while loop to collect a valid entry of y or n from the player
+    Gets restart choice input from the player (y or n).
+
+    Uses a while loop to collect a valid entry of 'y' or 'n' from the player
     via the terminal. The loop will repeatedly request data, until it is valid.
+
+    Args:
+        None
+
+    Returns:
+        str: Choice of y or n entered by the player
     """
     while True:
         print(Fore.WHITE + " Better luck next time!\n\n")
@@ -298,9 +364,16 @@ def get_restart_choice():
 
 def validate_restart_choice(value):
     """
-    Validates if the player enters a restart_choice value that is 'y' or 'n'.
+    Checks that the player enters a story choice value that is either y or n.
     Inside the try, raises a ValueError if the player enters a value that is
     not y or n.
+
+    Args:
+        value (str): The story_choice (y or n) entered by the player.
+
+    Returns:
+        boolean: True if validation runs with no error.
+        boolean: False if ValueError triggered.
     """
     try:
         if value not in ["y", "n"]:
@@ -314,6 +387,9 @@ def validate_restart_choice(value):
 
 
 def main():
+    """
+    Runs all of the main program functions in sequence.
+    """
     player_name = get_player_name()
     start_adventure(player_name)
     display_spider_story()
